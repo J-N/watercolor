0ae968b4c5e4ba2f503032b616427728d4b6636a
John-Nicholas <J-N@John-Nicholas.net>
2012-10-07 23:30:41 -0400

    Updates

--- canvas.py
+++ canvas.py
@@ -8,22 +8,79 @@ class Canvas:
 	def __init__(self,width,height):
 		self.width=width;
 		self.height=height;
+		for x in range (0,self.width-1):
+			for y in range (0,self.height-1):
+				self.cells[(x,y)] = Cell(x,y)
+		for x in range (0,self.width-1):
+			for y in range (0,self.height-1):
+				borderCell = Cell(-1,-1)
+				if(y==0):
+					self.cells[(x,y)].nCell=borderCell
+				else:
+					self.cells[(x,y)].nCell=self.cells[(x,y-1)])
+				if(x==0):
+					self.cells[(x,y)].wCell=borderCell
+				else:
+					self.cells[(x,y)].wCell=self.cells[(x-1,y)])
+				if(y==self.height-1):
+					self.cells[(x,y)].sCell=borderCell
+				else:
+					self.cells[(x,y)].sCell=self.cells[(x,y+1)])
+				if(x==self.width-1):
+					self.cells[(x,y)].eCell=borderCell
+				else:
+					self.cells[(x,y)].eCell=self.cells[(x+1,y)])
+
+	def deltaStep(self):
 		for x in range (0,self.width):
 			for y in range (0,self.height):
-				self.cells[(x,y)] = Cell(x,y)
-	def updateVelocities(M,u,v,p):
+				self.cells[(x,y)].deltaStep()
+	def getMaxVelocity(self):
 		max=0;
 		for x in range (0,self.width):
 			for y in range (0,self.height):
-				if(cells[(x,y)].getMaxVelocity > max)
-					max=cells[(x,y)].getMaxVelocity
-			
-		dt=1/max
+				if(self.cells[(x,y)].getMaxVelocity > max)
+					max=self.cells[(x,y)].getMaxVelocity
+		return max
+	
+	def updateVelocities(self,M,u,v,p):
+		self.deltaStep()
+		dt=1/self.getMaxVelocity()
 		for i in range (0,1,dt):
 			for x in range (0,self.width):
 				for y in range (0,self.height):
-					cells[(x,y)].
-
-		paperSlope=0
-		u=u-paperSlope
-		
+					self.cells[(x,y)].updateVelocity(dt)
+		for x in range (0,self.width):
+			for y in range (0,self.height):
+				if ((x,y) in M):
+					self.cells[(x,y)].nV=self.cells[(x,y)].northVPrime
+					self.cells[(x,y)].eU=self.cells[(x,y)].eastUPrime
+				else:
+					self.cells[(x,y)].nV=0
+					self.cells[(x,y)].eU=0
+					self.cells[(x,y)].sU=0
+					self.cells[(x,y)].wU=0
+					
+	def relaxDivergence(self,u,v,p):
+		t=0
+		while 1:
+			for x in range (0,self.width):
+				for y in range (0,self.height):
+					self.cells[(x,y)].eastUPrime=self.cells[(x,y)].eU
+					self.cells[(x,y)].westUPrime=self.cells[(x,y)].wU
+					self.cells[(x,y)].northVPrime=self.cells[(x,y)].nV
+					self.cells[(x,y)].southVPrime=self.cells[(x,y)].sV
+            deltaMax=0
+			for x in range (0,self.width):
+				for y in range (0,self.height):
+                    deltaMax=self.cells[(x,y)].relaxDivergence(deltaMax)
+			for x in range (0,self.width):
+				for y in range (0,self.height):
+					self.cells[(x,y)].eU=self.cells[(x,y)].eastUPrime
+					self.cells[(x,y)].wU=self.cells[(x,y)].westUPrime
+					self.cells[(x,y)].nV=self.cells[(x,y)].northVPrime
+					self.cells[(x,y)].sV=self.cells[(x,y)].southVPrime
+            t=t+1
+            if((deltMax<=.01)||(t>=50))
+                break
+        
--- cell.py
+++ cell.py
@@ -1,5 +1,8 @@
 import random
 class Cell:
+	mew=.1
+        drag=.01
+
 	x=0
 	y=0
 	h=0		#paper height at cell
@@ -21,10 +24,51 @@ class Cell:
 	sDT=0
 	eDT=0
 
+	nCell=0
+	wCell=0
+	sCell=0
+	eCell=0
+
+	eastUPrime=0
+	westUPrime=0
+	northVPrime=0	
+	southVPrime=0	
 	def __init__(self,x,y):
 		self.x=x
 		self.y=y
 		self.h=random.random()
 
-	def getMaxVelocity:
+	def deltaStep(self):
+		if((self.x>-1)&&(self.y>-1)):
+			self.nV=self.nv-self.nDT
+                	self.wU=self.wU-self.wDT
+                	self.sV=self.sV-self.sDT
+                	self.eU=self.eU-self.eDT
+		
+	def getMaxVelocity(self):
 		return max(self.nV,self.sV,self.wU,self.eU)
+	def getU(self):
+		return (self.wU+self.eU)/2
+	def getV(self):
+		return (self.nV+self.sV)/2
+	def updateVelocity(self,dt):
+		 u=self.getU()
+                 v=self.getV()
+
+                 A=u^2-self.eCell.getU()^2+(self.eU*self.sV)-(self.eU*self.nV)
+                 B=(self.eCell.eU+self.wU+self.nCell.eU+self.sCell.eU-(4*self.eU))
+                 self.eastUPrime=self.eU+dt(A-(mew*B)+self.pressure-eCell.pressure-(drag*self.eU))	
+		 	
+                 A=v^2-self.nCell.getV()^2+(self.wU*self.nV)-(self.eU*self.nV)
+                 B=(self.eCell.nV+self.wCell.sV+self.nCell.nV+self.sV-(4*self.nV))
+                 self.northVPrime=self.nV+dt(A-(mew*B)+self.pressure-nCell.pressure-(drag*self.nV))	
+	def relaxDivergence(self,deltaMax):
+		delta=.1*(self.eU-self.wU+self.nV+self.sV)
+		self.pressure=self.pressure+delta
+		self.eastUPrime=self.eastUPrime+delta
+		self.westUPrime=self.westUPrime-delta
+		self.northVPrime=self.northVprime+delta
+		self.southVPrime=self.southVprime-delta
+		return max(deltaMax,delta)
+			
+		
